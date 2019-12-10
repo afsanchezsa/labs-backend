eac138ff54dc847a4b822edc8ef38bba9300a707
unknown <afsanchezsa@unal.edu.co>
2019-12-09 19:04:18 -0500

    comment

--- src/test/java/co/edu/unal/software_engineering/labs/LabsApplicationTests.java
+++ src/test/java/co/edu/unal/software_engineering/labs/LabsApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith( SpringRunner.class )
 @SpringBootTest
 public class LabsApplicationTests{
-
+//the test were modified
     @Test
     public void contextLoads( ){ }
 
